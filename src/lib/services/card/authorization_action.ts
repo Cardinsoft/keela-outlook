376009c64b7038495622f471578cbb6f7ee8b77b
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2022-10-18 22:13:37 +0300

    added AuthorizationAction lib class

---
+++ src/lib/services/card/authorization_action.ts
@@ -0,0 +1,17 @@
+/**
+ * @see https://developers.google.com/apps-script/reference/card-service/authorization-action
+ */
+class AuthorizationAction {
+  url?: string;
+
+  /**
+   * @see https://developers.google.com/apps-script/reference/card-service/authorization-action#setauthorizationurlauthorizationurl
+   *
+   * @summary Sets the authorization URL that user is taken to from the authorization prompt. Required.
+   * @param authorizationUrl The authorization URL to set.
+   */
+  setAuthorizationUrl(authorizationUrl: string) {
+    this.url = authorizationUrl;
+    return this;
+  }
+}
