2ff50e574055b137f13448dfc94d755571ab63b5
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2022-10-19 01:43:51 +0300

    added SuggestionsResponse lib class

---
+++ src/lib/services/card/suggestions_response.ts
@@ -0,0 +1,8 @@
+namespace Components {
+  /**
+   * @see https://developers.google.com/apps-script/reference/card-service/suggestions-response
+   */
+  export class SuggestionsResponse extends CardServiceComponent {
+    suggestions?: Suggestions;
+  }
+}
