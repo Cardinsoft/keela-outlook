6d482408514b83d27142e42dfa4f353e1cc8ede6
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2022-10-18 22:41:04 +0300

    made Navigation a part of the Components namespace

--- src/lib/services/card/navigation.ts
+++ src/lib/services/card/navigation.ts
@@ -1,76 +1,74 @@
-/**
- * @see https://developers.google.com/apps-script/reference/card-service/navigation
- */
-class Navigation extends CardServiceComponent {
-  private static stack: Card[] = [];
-
+namespace Components {
   /**
-   * @see https://developers.google.com/apps-script/reference/card-service/navigation#popcard
-   *
-   * @summary Pops a card from the navigation stack.
+   * @see https://developers.google.com/apps-script/reference/card-service/navigation
    */
-  popCard() {
-    Navigation.stack.pop();
-    return this;
-  }
+  export class Navigation extends CardServiceComponent {
+    private static stack: Card[] = [];
 
-  /**
-   * @see https://developers.google.com/apps-script/reference/card-service/navigation#poptonamedcardcardname
-   *
-   * @summary Pops to the specified card by its card name.
-   * @param cardName The name of the card to navigate to.
-   */
-  popToNamedCard(cardName: string) {
-    const { stack } = Navigation;
-
-    for (let i = stack.length - 1; i >= 0; i--) {
-      const card = stack[i];
-
-      if (card.name === cardName) break;
-
-      stack.pop();
+    /**
+     * @see https://developers.google.com/apps-script/reference/card-service/navigation#popcard
+     *
+     * @summary Pops a card from the navigation stack.
+     */
+    popCard() {
+      Navigation.stack.pop();
+      return this;
     }
 
-    return this;
-  }
+    /**
+     * @see https://developers.google.com/apps-script/reference/card-service/navigation#poptonamedcardcardname
+     *
+     * @summary Pops to the specified card by its card name.
+     * @param cardName The name of the card to navigate to.
+     */
+    popToNamedCard(cardName: string) {
+      const { stack } = Navigation;
 
-  /**
-   * @see https://developers.google.com/apps-script/reference/card-service/navigation#poptoroot
-   *
-   * @summary Pops the card stack to the root card.
-   */
-  popToRoot() {
-    const { stack } = Navigation;
+      for (let i = stack.length - 1; i >= 0; i--) {
+        const card = stack[i];
 
-    const { length } = stack;
-    for (let i = 1; i < length; i++) {
-      stack.pop();
+        if (card.name === cardName) break;
+
+        stack.pop();
+      }
+
+      return this;
     }
 
-    return this;
-  }
+    /**
+     * @see https://developers.google.com/apps-script/reference/card-service/navigation#poptoroot
+     *
+     * @summary Pops the card stack to the root card.
+     */
+    popToRoot() {
+      const { stack } = Navigation;
 
-  /**
-   * @see https://developers.google.com/apps-script/reference/card-service/navigation#pushcardcard
-   *
-   * @summary Pushes the given card onto the stack.
-   * @param card A card to add to the stack.
-   */
-  pushCard(card: Card) {
-    Navigation.stack.push(card);
-    return this;
-  }
+      const { length } = stack;
+      for (let i = 1; i < length; i++) {
+        stack.pop();
+      }
 
-  /**
-   * @summary Does an in-place replacement of the current card.
-   * @param card A card to replace the current card with.
-   */
-  updateCard(card: Card) {
-    Navigation.stack.splice(-1, 1, card);
-    return this;
+      return this;
+    }
+
+    /**
+     * @see https://developers.google.com/apps-script/reference/card-service/navigation#pushcardcard
+     *
+     * @summary Pushes the given card onto the stack.
+     * @param card A card to add to the stack.
+     */
+    pushCard(card: Card) {
+      Navigation.stack.push(card);
+      return this;
+    }
+
+    /**
+     * @summary Does an in-place replacement of the current card.
+     * @param card A card to replace the current card with.
+     */
+    updateCard(card: Card) {
+      Navigation.stack.splice(-1, 1, card);
+      return this;
+    }
   }
 }
-
-// Navigation.prototype.popToNamedCard = function (cardName) {
-//   //future releases;
-// };
