38cf4912cee6c1110a62dc9d657e8cbd119f66ce
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2022-10-19 15:23:34 +0300

    added CacheService lib class

---
+++ src/lib/services/cache/service.ts
@@ -0,0 +1,40 @@
+enum CacheType {
+  DOCUMENT = "document",
+  SCRIPT = "script",
+  USER = "user",
+}
+
+/**
+ * @see https://developers.google.com/apps-script/reference/cache/cache-service
+ */
+class CacheService {
+  /**
+   * @see https://developers.google.com/apps-script/reference/cache/cache-service#getdocumentcache
+   *
+   * @summary Gets the cache instance scoped to the current document and script.
+   */
+  static getDocumentCache() {
+    const { localStorage } = window;
+    return new Components.Cache(localStorage, CacheType.DOCUMENT);
+  }
+
+  /**
+   * @see https://developers.google.com/apps-script/reference/cache/cache-service#getscriptcache
+   *
+   * @summary Gets the cache instance scoped to the script.
+   */
+  static getScriptCache() {
+    const { localStorage } = window;
+    return new Components.Cache(localStorage, CacheType.SCRIPT);
+  }
+
+  /**
+   * @see https://developers.google.com/apps-script/reference/cache/cache-service#getusercache
+   *
+   * @summary Gets the cache instance scoped to the current user and script.
+   */
+  static getUserCache() {
+    const { localStorage } = window;
+    return new Components.Cache(localStorage, CacheType.USER);
+  }
+}
