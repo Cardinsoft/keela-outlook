03cdf1a04df3a923f026dde77e0fffe66bb6c461
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2022-10-19 23:46:29 +0300

    added TextButton lib class

---
+++ src/lib/services/card/components/text_button.ts
@@ -0,0 +1,77 @@
+namespace Components {
+  /**
+   * @see https://developers.google.com/apps-script/reference/card-service/text-button
+   */
+  export class TextButton extends Button {
+    altText?: string;
+    backgroundColor: string = "#0078d7";
+    disabled: boolean = false;
+    text?: string;
+    textButtonStyle: TextButtonStyle = TextButtonStyle.TEXT;
+
+    setAltText(altText: string) {
+      this.altText = altText;
+      return this;
+    }
+
+    setBackgroundColor(backgroundColor: string) {
+      this.backgroundColor = backgroundColor;
+      return this;
+    }
+
+    setDisabled(disabled: boolean) {
+      this.disabled = disabled;
+      return this;
+    }
+
+    setText(text: string) {
+      this.text = text;
+      return this;
+    }
+
+    setTextButtonStyle(textButtonStyle: TextButtonStyle) {
+      this.textButtonStyle = textButtonStyle;
+      return this;
+    }
+
+    create(): HTMLElement {
+      const {
+        action,
+        backgroundColor,
+        disabled,
+        textButtonStyle,
+        text = "",
+      } = this;
+
+      const wrapper = document.createElement("div");
+
+      const button = document.createElement("div");
+      button.classList.add("TextButton");
+      button.classList.toggle("btn-disabled", disabled);
+
+      const filled = textButtonStyle === TextButtonStyle.FILLED;
+      if (filled) {
+        button.style.backgroundColor = backgroundColor;
+      }
+
+      button.classList.add(filled ? "btn-filled" : "btn-text");
+
+      parseHTMLWidgetContent(button, text);
+
+      if (!action) {
+        throw new Error(`TextButton must have at least one action set`);
+      }
+
+      ActionStore.set(button, action);
+
+      button.addEventListener("click", () => handleEvent(button));
+
+      wrapper.append(button);
+      return wrapper;
+    }
+
+    render(parentId: string): Promise<HTMLElement> {
+      return super.render(parentId);
+    }
+  }
+}
