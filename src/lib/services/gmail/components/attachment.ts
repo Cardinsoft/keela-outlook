3d43f54ba16fa21c5fbe45e6a3136ce793415e5e
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2022-10-20 20:31:27 +0300

    GmailAttachment lib class should accept Graph Attachment resource on init

--- src/lib/services/gmail/components/attachment.ts
+++ src/lib/services/gmail/components/attachment.ts
@@ -3,13 +3,17 @@ namespace Components {
    * @see https://developers.google.com/apps-script/reference/gmail/gmail-attachment
    */
   export class GmailAttachment extends _Blob {
+    constructor(private item: microsoftgraph.Attachment) {
+      super();
+    }
+
     /**
      * @see https://developers.google.com/apps-script/reference/gmail/gmail-attachment#getsize
      *
      * @summary Gets the size of this attachment.
      */
     getSize() {
-      return this.getBytes().length;
+      return this.item.size!;
     }
   }
 }
