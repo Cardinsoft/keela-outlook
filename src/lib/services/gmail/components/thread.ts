43bbf1665a57029340e8104acdcc345034145ef1
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2022-10-20 20:41:03 +0300

    added GmailThread lib class (mostly just an adapter)

---
+++ src/lib/services/gmail/components/thread.ts
@@ -0,0 +1,38 @@
+namespace Components {
+  /**
+   * @see https://developers.google.com/apps-script/reference/gmail/gmail-thread
+   */
+  export class GmailThread {
+    constructor(protected item: microsoftgraph.ConversationThread) {}
+
+    async moveToArchive() {
+      // TODO: future releases
+      return this;
+    }
+
+    async moveToInbox() {
+      // TODO: future releases
+      return this;
+    }
+
+    async moveToSpam() {
+      // TODO: future releases
+      return this;
+    }
+
+    async moveToTrash() {
+      // TODO: future releases
+      return this;
+    }
+
+    async refresh() {
+      // TODO: future releases
+      return this;
+    }
+
+    async removeLabel(_label: Components.GmailLabel) {
+      // TODO: future releases
+      return this;
+    }
+  }
+}
