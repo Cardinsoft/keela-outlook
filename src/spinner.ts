99960d4788a24e2ebefb4a3b84665044188fb1b9
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2022-10-20 22:03:57 +0300

    added Spinner ui class

---
+++ src/spinner.ts
@@ -0,0 +1,31 @@
+type SpinnerSize = "large" | "small";
+
+class Spinner extends Component {
+  private size: SpinnerSize = "small";
+
+  setSize(size: SpinnerSize) {
+    const { element, size: oldSize } = this;
+
+    this.size = size;
+
+    element.classList.remove(`spinner-${oldSize}`);
+    element.classList.add(`spinner-${size}`);
+
+    return this;
+  }
+
+  create(parentId: string) {
+    const { size } = this;
+
+    const spinner = document.createElement("div");
+    spinner.classList.add("spinner", `spinner-${size}`);
+
+    const parent = document.getElementById(parentId);
+    if (!parent) {
+      throw new Error(`missing spinner parent: #${parentId}`);
+    }
+
+    parent.append(spinner);
+    return spinner;
+  }
+}
