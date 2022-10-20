5ac2b223e0f5745b9e486ac57ba3e1c79ee4f31f
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2022-10-20 22:04:45 +0300

    added mailbox utilities

---
+++ src/utils/mailbox.ts
@@ -0,0 +1,32 @@
+type MessageFormParameters = {
+  attachments: Array<{
+    isInline: boolean;
+    itemId: string;
+    name: string;
+    type: Office.MailboxEnums.AttachmentType;
+    url: string;
+  }>;
+  bccRecipients: Array<string | Office.EmailAddressDetails>;
+  ccRecipients: Array<string | Office.EmailAddressDetails>;
+  htmlBody: string;
+  subject: string;
+  toRecipients: Array<string | Office.EmailAddressDetails>;
+};
+
+/**
+ * @summary intercepts mailto link clicks to display new message form
+ * @param target target link
+ * @param to recipient email address
+ */
+const addMailtoListener = (target: EventTarget, to: string) => {
+  target.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const params: Partial<MessageFormParameters> = {
+      toRecipients: [to],
+    };
+
+    Office.context.mailbox.displayNewMessageForm(params);
+    return false;
+  });
+};
