272f723c743fdbc1adc1d8077f0eb27676d9c3c9
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2022-10-21 21:33:31 +0300

    added appsscript.json schema definition as type

---
+++ src/manifest.d.ts
@@ -0,0 +1,57 @@
+interface AppsScriptManifest {
+  timeZone: string;
+  dependencies: {
+    enabledAdvancedServices?: Array<{
+      serviceId: string;
+      userSymbol: string;
+      version: string;
+    }>;
+    libraries?: Array<{
+      developmentMode?: boolean;
+      libraryId: string;
+      userSymbol: string;
+      version: string;
+    }>;
+  };
+  exceptionLogging: "STACKDRIVER" | "NONE";
+  runtimeVersion?: "DEPRECATED_ES5" | "STABLE" | "V8";
+  oauthScopes: string[];
+  urlFetchWhitelist?: string[];
+}
+
+interface AddOnManifest extends AppsScriptManifest {
+  addOns: {
+    common: {
+      homepageTrigger: {
+        enabled?: boolean;
+        runFunction: string;
+      };
+      universalActions: Array<{ label: string; runFunction: string }>;
+      openLinkUrlPrefixes: string[];
+      logoUrl: string;
+      name: string;
+      layoutProperties: {
+        secondaryColor: string;
+        primaryColor: string;
+      };
+    };
+  };
+}
+
+interface GmailAddOnManifest extends AddOnManifest {
+  addOns: {
+    common: AddOnManifest["addOns"]["common"];
+    gmail: {
+      homepageTrigger: {
+        enabled?: boolean;
+        runFunction: string;
+      };
+      contextualTriggers: [
+        {
+          unconditional: {};
+          onTriggerFunction: string;
+        }
+      ];
+    };
+  };
+}
