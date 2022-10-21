f1c6bf6f22a6234be0ff3754fb05b93f02d73a33
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2022-10-21 20:24:12 +0300

    added function utilities

---
+++ src/utils/functions.ts
@@ -0,0 +1,12 @@
+/**
+ * @summary looks up and call function in the global scope
+ * @param name function name
+ * @param event event object
+ */
+const callFunctionFromGlobalScope = <T>(
+  name: string,
+  event: EventObject
+): T => {
+  const func = window[name];
+  return typeof func === "function" ? func(event) : void 0;
+};
