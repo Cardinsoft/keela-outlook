4cea65a7a315efdf48a5869e92df5d7beca4bc12
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2022-10-22 00:15:05 +0300

    added ImageCropStyle lib class

---
+++ src/lib/services/card/components/image_crop_style.ts
@@ -0,0 +1,41 @@
+namespace Components {
+  /**
+   * @see https://developers.google.com/apps-script/reference/card-service/image-crop-style
+   */
+  export class ImageCropStyle {
+    ratio?: number;
+    type: ImageCropType = ImageCropType.SQUARE;
+
+    /**
+     * @see https://developers.google.com/apps-script/reference/card-service/image-crop-style#setaspectratioratio
+     *
+     * @summary Sets the aspect ratio to use if the crop type is {@link ImageCropType.RECTANGLE_CUSTOM}.
+     * @param ratio
+     */
+    setAspectRatio(ratio: number) {
+      const { type } = this;
+
+      if (type !== ImageCropType.RECTANGLE_CUSTOM) {
+        return this;
+      }
+
+      if (ratio < 0) {
+        throw new Error("aspect ratio must be a positive value");
+      }
+
+      this.ratio = ratio;
+      return this;
+    }
+
+    /**
+     * @see https://developers.google.com/apps-script/reference/card-service/image-crop-style#setimagecroptypetype
+     *
+     * @summary Sets the crop type for the image. Default is {@link ImageCropType.SQUARE}.
+     * @param type The crop type.
+     */
+    setImageCropType(type: ImageCropType) {
+      this.type = type;
+      return this;
+    }
+  }
+}
