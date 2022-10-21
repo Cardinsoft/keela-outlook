2fa388c7495930a8a0780cd369367966f31a5bd8
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2022-10-21 20:20:06 +0300

    added UnionToIntersection utility type

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,7 @@
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
+  k: infer I
+) => void
+  ? I
+  : never;
+
 declare var fabric: typeof import("office-ui-fabric-js/dist/js/fabric.js").fabric;
