3cbfd59a48c01139fd6de910e2d1101c3bd4d811
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2022-10-21 20:20:44 +0300

    added string index signature to Window interface

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,4 +4,9 @@ type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
   ? I
   : never;
 
+interface Window {
+  // required to access action callback functions
+  [x: string]: unknown;
+}
+
 declare var fabric: typeof import("office-ui-fabric-js/dist/js/fabric.js").fabric;
