c8ebaa8a21935a21678efa021edd9b5563ed6de8
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2022-10-21 19:54:40 +0300

    added TextParagraph lib class

---
+++ src/lib/services/card/components/text_paragraph.ts
@@ -0,0 +1,38 @@
+namespace Components {
+  /**
+   * @see https://developers.google.com/apps-script/reference/card-service/text-paragraph
+   */
+  export class TextParagraph extends CardServiceRenderableComponent {
+    private text?: string;
+
+    /**
+     * @see https://developers.google.com/apps-script/reference/card-service/text-paragraph#settexttext
+     *
+     * @summary Sets the text of the paragraph. Required.
+     * @param text The text to display.
+     */
+    setText(text: string) {
+      this.text = text;
+      return this;
+    }
+
+    create(): HTMLElement {
+      const { text } = this;
+
+      if (!text) {
+        throw new Error("TextParagraph must have text set");
+      }
+
+      const wrapper = document.createElement("div");
+      wrapper.classList.add("row");
+
+      const contentText = document.createElement("span");
+      contentText.classList.add("ms-font-m-plus");
+      wrapper.append(contentText);
+
+      parseHTMLWidgetContent(contentText, text);
+
+      return wrapper;
+    }
+  }
+}
