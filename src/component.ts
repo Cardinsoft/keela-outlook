a390594e9d8d3ad0ac973bb733453a3c9b907850
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2022-10-23 00:27:57 +0300

    removed obsolete Component class

--- src/component.ts
+++ src/component.ts
@@ -1,41 +1,5 @@
 import { InspectableComponent } from "./lib/services/card/index";
 
-export abstract class Component {
-  /**
-   * @summary component element
-   */
-  protected element: HTMLElement;
-
-  /**
-   * @param parentId parent element id
-   */
-  constructor(parentId: string) {
-    this.element = this.create(parentId);
-  }
-
-  /**
-   * @summary creates the component {@link Component.element}
-   * @param parentId parent element id
-   */
-  abstract create(parentId: string): HTMLElement;
-
-  /**
-   * @summary hides the component
-   */
-  hide() {
-    this.element.hidden = true;
-    return this;
-  }
-
-  /**
-   * @summary shows the component
-   */
-  show() {
-    this.element.hidden = false;
-    return this;
-  }
-}
-
 export abstract class RenderableComponent extends InspectableComponent {
   /**
    * @summary component element
