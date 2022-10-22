968575dd51fd2fe93e342f41e773294a8de7a441
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2022-10-22 12:50:31 +0300

    added UrlFetchApp service utils

---
+++ src/lib/services/url_fetch/utils.ts
@@ -0,0 +1,13 @@
+/**
+ * @summary converts UrlFetchApp payload to {@link FormData}
+ * @param payload payload to convert
+ */
+const payloadToFormData = (
+  payload: Exclude<UrlFetchAdvancedParameters["payload"], undefined>
+) => {
+  const formData = new FormData();
+  Object.entries(payload).forEach(([key, value]) => {
+    formData.append(key, value);
+  });
+  return formData;
+};
