2c6290b96138daac02acd8c04fe3b43f905ce8a1
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2022-10-24 22:25:01 +0300

    improved safeToString util

--- src/gas-js/utils/strings.ts
+++ src/gas-js/utils/strings.ts
@@ -1,7 +1,23 @@
+type ToStringHandler = {
+  [P in string | { toString(): string } as string]: (item: P) => string;
+}[string];
+
+type ToStringRule = [boolean, ToStringHandler];
+
 /**
  * @summary override-safe default stringifier
  * @param item item to stringify
  */
 export const safeToString = (item: unknown) => {
-  return typeof item === "string" ? item : Object.prototype.toString.call(item);
+  const rules: ToStringRule[] = [
+    [typeof item === "string", (item: string) => item],
+    [
+      typeof item === "object" && !!item && "toString" in item,
+      (item: { toString(): string }) => item.toString(),
+    ],
+  ];
+
+  const [, handler] = rules.find(([condition]) => !!condition) || [];
+
+  return handler?.(item as string) || Object.prototype.toString.call(item);
 };
