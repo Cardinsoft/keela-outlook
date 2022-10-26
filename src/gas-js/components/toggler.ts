beabdea1f83a23e07752e9964410618847591855
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2022-10-27 01:12:36 +0300

    updated Toggler lib class rendering logic

--- src/gas-js/components/toggler.ts
+++ src/gas-js/components/toggler.ts
@@ -8,26 +8,29 @@ export class Toggler extends RenderableComponent {
    * @summary sets a callback to run on toggle
    * @param callback callback to run
    */
-  setOnToggle(callback: (element: HTMLElement) => void) {
+  setOnToggle(callback: (element: HTMLElement) => Promise<void>) {
     const element = (this.element ||= this.create());
 
     element.addEventListener("click", async () => {
-      callback(element);
-      element.classList.toggle("toggler-up");
+      element.classList.toggle("card-section-toggler--up");
+      await callback(element);
     });
   }
 
   create(): HTMLElement {
-    const toggler = document.createElement("div");
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("card-section-toggler");
 
-    toggler.classList.add(
-      "toggler",
-      "centered",
+    const shevron = document.createElement("div");
+    shevron.classList.add(
+      "card-section-toggler-icon",
       "ms-Icon",
       "ms-Icon--ChevronDown",
       "pointer"
     );
+    
+    wrapper.append(shevron);
 
-    return toggler;
+    return wrapper;
   }
 }
