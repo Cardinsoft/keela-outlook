77b5d6061def140c5e79cf21e6b8b80da42e2b3d
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2022-10-27 15:07:55 +0300

    finalized build step

--- scripts/gulpfile.ts
+++ scripts/gulpfile.ts
@@ -8,6 +8,7 @@ import postcss from "gulp-postcss";
 import replace from "gulp-replace";
 import gsass from "gulp-sass";
 import uglify from "gulp-uglify";
+import { basename } from "path";
 import sass from "sass";
 import tsify from "tsify";
 import buffer from "vinyl-buffer";
@@ -15,34 +16,36 @@ import source from "vinyl-source-stream";
 
 import config from "./gulp.config.json";
 
+process.chdir("..");
+
 export const buildCSS = () => {
   return gulp
     .src(config.paths.src.css)
     .pipe(gsass(sass)())
     .pipe(postcss([autoprefixer(), cssnano()]))
-    .pipe(gulp.dest("../dist"))
+    .pipe(gulp.dest("dist"))
     .on("error", (args) => {
       console.debug(args);
     });
 };
 
-export const buildJS = () => {
+export const buildJS = (entryPath: string, bundleName: string) => () => {
   return browserify({
     debug: true,
-    entries: [config.paths.src.ts.entry.lib],
+    entries: [entryPath],
     cache: {},
     packageCache: {},
   })
     .plugin(tsify)
     .bundle()
-    .pipe(source("bundle.js"))
+    .pipe(source(bundleName))
     .pipe(buffer())
     .pipe(uglify({ mangle: false, output: { webkit: true } }))
-    .pipe(gulp.dest("../dist"));
+    .pipe(gulp.dest("dist"));
 };
 
 export const injectLocalCSS = () => {
-  return inject(gulp.src(config.paths.dist.css, { cwd: ".." }), {
+  return inject(gulp.src(config.paths.dist.css), {
     removeTags: true,
     transform: (_path, file) => {
       return `<style>\n${file?.contents?.toString("utf-8") || ""}\n</style>`;
@@ -52,16 +55,12 @@ export const injectLocalCSS = () => {
 
 export const injectLocalJS = () => {
   return inject(
-    gulp.src(
-      [
-        config.paths.external.cardin,
-        config.paths.external.keela,
-        config.paths.dist.js,
-      ],
-      {
-        cwd: "..",
-      }
-    ),
+    gulp.src([
+      config.paths.dist.js.gas,
+      config.paths.external.cardin,
+      config.paths.external.keela,
+      config.paths.dist.js.entry,
+    ]),
     {
       removeTags: true,
       transform: (_path, file) => {
@@ -101,30 +100,31 @@ export const bundle = () => {
     .pipe(injectRemoteJS())
     .pipe(injectLocalJS())
     .pipe(injectLocalCSS())
-    .pipe(gulp.dest("../dist"));
+    .pipe(gulp.dest("dist"));
 };
 
 export const cleanDist = () => {
-  return del(
-    ["../dist/*", "!../dist/*.html", "!../dist/assets", "!../dist/public"],
-    {
-      force: true,
-    }
-  );
+  return del(["dist/*", "!dist/*.html", "!dist/assets", "!dist/public"], {
+    force: true,
+  });
 };
 
 export const copyAssets = () => {
-  return gulp.src(config.paths.src.assets).pipe(gulp.dest("../dist/assets"));
+  return gulp.src(config.paths.src.assets).pipe(gulp.dest("dist/assets"));
 };
 
 export const copyIcons = () => {
-  return gulp
-    .src(config.paths.src.icons)
-    .pipe(gulp.dest("../dist/public/icons"));
+  return gulp.src(config.paths.src.icons).pipe(gulp.dest("dist/public/icons"));
 };
 
 export default gulp.series(
-  gulp.parallel(buildCSS, buildJS, copyAssets, copyIcons),
+  gulp.parallel(
+    buildCSS,
+    buildJS(config.paths.src.ts.gas, basename(config.paths.dist.js.gas)),
+    buildJS(config.paths.src.ts.entry, basename(config.paths.dist.js.entry)),
+    copyAssets,
+    copyIcons
+  ),
   bundle,
   cleanDist
 );
