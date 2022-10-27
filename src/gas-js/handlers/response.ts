a8a3d092bd8282a4e083cc109ea9a9156b950e14
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2022-10-27 15:06:39 +0300

    reworked response handlers to use CardStore lib class

--- src/gas-js/handlers/response.ts
+++ src/gas-js/handlers/response.ts
@@ -1,29 +1,97 @@
+import {
+  NavigationAction,
+  NavigationType,
+  PushNavigationAction,
+} from "../services/card/components/navigation";
 import { type ActionResponse } from "../services/card/components/responses/action_response";
 import { type SuggestionsResponse } from "../services/card/components/responses/suggestions_response";
 import { type UniversalActionResponse } from "../services/card/components/responses/universal_action_response";
-import { CardServiceConfig } from "../services/card/service";
+import { type CardStore } from "../stores/card";
+import { RuleStore } from "../stores/rules";
+import { log } from "../utils/log";
+import { safeToString } from "../utils/strings";
 import { handleOpenLinkAction } from "./open_link";
+
+type NavigationHandler<T extends NavigationAction> = (
+  store: typeof CardStore,
+  action: T
+) => Promise<void> | void;
+
+type NavigationHandlerRule = {
+  [P in NavigationType as string]: [
+    P,
+    NavigationHandler<
+      P extends NavigationType.PUSH ? PushNavigationAction : NavigationAction
+    >
+  ];
+}[string];
+
+const navigationRules = new RuleStore<NavigationHandlerRule>([
+  [
+    NavigationType.NAMED,
+    (store: typeof CardStore, action: NavigationAction) => {
+      store.named(action.name!);
+    },
+  ],
+  [
+    NavigationType.POP,
+    (store: typeof CardStore) => {
+      store.pop();
+    },
+  ],
+  [
+    NavigationType.PUSH,
+    (store: typeof CardStore, action: PushNavigationAction) => {
+      store.push(action.card);
+    },
+  ],
+  [
+    NavigationType.ROOT,
+    (store: typeof CardStore) => {
+      store.root();
+    },
+  ],
+]);
 
 /**
  * @summary handles an {@link ActionResponse}
  * @param response {@link ActionResponse} to handle
  */
 export const handleActionResponse = async (response: ActionResponse) => {
-  const { navigation, notification, openLink } = response;
+  try {
+    const { navigation, notification, openLink } = response;
 
-  if (navigation) {
-    await CardServiceConfig.lastCard.render(
-      document.getElementById("app-body")
-    );
-  }
+    if (navigation) {
+      const { actions } = navigation;
 
-  if (notification) {
-    const parent = document.getElementById("app-notif") || document.body;
-    await notification.render(parent);
-  }
+      const { CardStore } = window.CardServiceConfig;
 
-  if (openLink) {
-    handleOpenLinkAction(openLink);
+      await CardStore.teardown();
+
+      for (const action of actions) {
+        const { type } = action;
+
+        const [, navigationHandler] = navigationRules.find(([t]) => t === type);
+        if (!navigationHandler) {
+          throw new Error(`missing Navigation handler`, { cause: action });
+        }
+
+        navigationHandler(CardStore, action as PushNavigationAction);
+      }
+
+      await CardStore.render(document.getElementById("app-body"));
+    }
+
+    if (notification) {
+      const parent = document.getElementById("app-notif") || document.body;
+      await notification.render(parent);
+    }
+
+    if (openLink) {
+      handleOpenLinkAction(openLink);
+    }
+  } catch (error) {
+    log("error", "ActionResponse handler error", safeToString(error));
   }
 };
 
@@ -51,16 +119,21 @@ export const handleSuggestionsResponse = (
 export const handleUniversalActionResponse = async (
   response: UniversalActionResponse
 ) => {
-  const { cards, openLink } = response;
+  try {
+    const { cards, openLink } = response;
 
-  if (cards.length) {
-    await CardServiceConfig.resetCardStack(cards);
-    await CardServiceConfig.lastCard.render(
-      document.getElementById("app-body")
-    );
-  }
+    const { CardStore } = window.CardServiceConfig;
 
-  if (openLink) {
-    handleOpenLinkAction(openLink);
+    if (cards.length) {
+      await CardStore.teardown();
+      CardStore.reset(cards);
+      await CardStore.render(document.getElementById("app-body"));
+    }
+
+    if (openLink) {
+      handleOpenLinkAction(openLink);
+    }
+  } catch (error) {
+    log("error", "UniversalActionResponse handler error", safeToString(error));
   }
 };
